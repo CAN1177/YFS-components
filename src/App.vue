551680ecc8dfaf5b892b67213e3a3eb7647039e3
CAN7 <39960526+CAN1177@users.noreply.github.com>
2021-06-27 17:53:28 +0800

    feat: 评分星星、switches切换组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <!--
  * @Author: CAN1177
  * @Date: 2021-06-27 09:37:08
- * @LastEditTime: 2021-06-27 15:09:54
+ * @LastEditTime: 2021-06-27 17:36:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /yfs-component/src/App.vue
 -->
 <template>
   <div id="app">
-    <h1 class="title">CAN1177常用组件合集</h1>
-    
+    <h1 class="title">CAN1177常用组件合集<a href="https://github.com/CAN1177/YFS-components">GitHub地址</a></h1>
+
     <div class="container">
-      <h3>基于ElementUI二次封装的季度组件</h3>
+      <h3>1、基于ElementUI二次封装的季度组件</h3>
       <season @value="selectValue" />
 
-      <h3>SVG环形进度条</h3>
+      <h3>2、SVG环形进度条</h3>
       <progress-circle :size="100" :percent="0.4" />
 
-      <h3>ElementU下拉多选增加 <span style="color:skyblue">全选</span> 选项</h3>
+      <h3>3、ElementU下拉多选增加 <span style="color:skyblue">全选</span> 选项</h3>
       <select-all @selectedArray="selectedValue" :options="options" />
 
-       <h3>ElementUI 多图上传拖拽排序</h3>
-       <!-- <img-upload v-model="formData.imgList"/> -->
+      <h3>4、ElementUI 多图上传拖拽排序</h3>
+      <!-- <img-upload v-model="formData.imgList"/> -->
+
+      <h3>5、Star 评分组件</h3>
+      <star :starNum="5" @starMarkChange="starChange" />
+
+
+       <h3>6、Switchs组件</h3>
+      <switches
+        @switch="switchItem"
+        :switches="switches"
+        :currentIndex="currentIndex"
+      ></switches>
     </div>
   </div>
 </template>
@@ -31,6 +42,8 @@ import Season from "./components/Season.vue";
 import ProgressCircle from "./components/ProgressCircle";
 import SelectAll from "./components/AllSelectDropDown";
 // import ImgUpload from "./components/ImgUpload"
+import Star from "./components/Star";
+import Switches from "./components/Switches";
 
 export default {
   name: "App",
@@ -39,9 +52,11 @@ export default {
     ProgressCircle, // svg环形进度条
     SelectAll, // 下拉全选
     // ImgUpload, //多图上传
+    Star, //star 评分组件
+    Switches, //switch 切换
   },
   data() {
-    return{
+    return {
       options: [
         { name: "梅西", label: "one" },
         { name: "C罗", label: "tow" },
@@ -50,9 +65,18 @@ export default {
         { name: "小罗", label: "five" },
       ],
       formData: {
-        imgList: [] // formData初始值要带上imgList属性，以保证数据响应式
-      }
-    }
+        imgList: [], // formData初始值要带上imgList属性，以保证数据响应式
+      },
+      currentIndex: 0,
+      switches: [
+        {
+          title: "选项1",
+        },
+        {
+          title: "选项2",
+        },
+      ],
+    };
   },
   methods: {
     selectValue(value) {
@@ -60,6 +84,13 @@ export default {
     },
     selectedValue(value) {
       console.log("选择的项目是： ", value);
+    },
+    starChange(value) {
+      console.log("你的分数是", `${value}分`);
+    },
+    switchItem(index) {
+      console.log('第几个switch按钮: ', index);
+      this.currentIndex = index;
     },
   },
 };
---
+++ src/components/Switches.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul class="switch">
+    <li
+      class="switch-item"
+      :class="{ active: index === currentIndex }"
+      @click="selectItem(index)"
+      v-for="(item, index) in switches"
+      :key="index"
+    >
+      {{ item.title }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['switches', 'currentIndex'],
+  methods: {
+    selectItem(index) {
+      this.$emit('switch', index);
+    }
+  }
+};
+</script>
+
+<style>
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+.switch {
+  display: flex;
+  width: 200px;
+  border: 3px solid #333;
+}
+
+.switch-item {
+  flex: 1;
+  padding: 10px;
+  text-align: center;
+}
+
+.active {
+  background-color: #333;
+  color: #fff;
+}
+</style>
