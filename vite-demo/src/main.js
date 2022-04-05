fbe431fe733bcad477423f1e2e834752aa40ad87
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-04-05 21:33:40 +0800

    feat: Count使用Vuex

--- vite-demo/src/main.js
+++ vite-demo/src/main.js
@@ -2,4 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import router from "./router/index";
-createApp(App).use(router).mount("#app");
+import store from "./store/index"
+// createApp(App).use(router).mount("#app");
+
+
+const app = createApp(App)
+app.use(store)
+    .use(router)
+    .mount('#app')
--- vite-demo/src/pages/home.vue
+++ vite-demo/src/pages/home.vue
@@ -1,9 +1,12 @@
 <template>
   <Fragment>
     <TodoList />
+    <hr/>
+    <Count/>
   </Fragment>
 </template>
 
 <script setup>
 import TodoList from "../components/TodoList.vue";
+import Count from "../components/Count.vue";
 </script>
