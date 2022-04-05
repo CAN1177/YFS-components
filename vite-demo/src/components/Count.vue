8a4c74f2cfe0b44778f84d317e272e46a53e9b2b
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-04-05 21:46:56 +0800

    feat:插件机制\reactive实现迷你Vuex

--- vite-demo/src/components/Count.vue
+++ vite-demo/src/components/Count.vue
@@ -6,7 +6,7 @@
 
 <script setup>
 import { computed } from "vue";
-import { useStore } from "vuex";
+import { useStore } from "../store/gvuex";
 let store = useStore();
 let count = computed(() => store.state.count);
 
--- vite-demo/src/store/index.js
+++ vite-demo/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from 'vuex'
+import { createStore } from './gvuex'
 
 
 const store = createStore({
