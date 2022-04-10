cc9fd80726ad8fb4e95abcee3cb34f69048eeea7
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-04-10 16:26:03 +0800

    feat: 打包体积分析

--- vite-demo/vite.config.js
+++ vite-demo/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  chainWebpack: config => {
+  plugins: [vue(), visualizer()],
+  chainWebpack: (config) => {
     // 修复HMR
     config.resolve.symlinks(true);
-
-},
-})
+  },
+});
