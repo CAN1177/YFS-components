4031dafd69d1bcab10fa353fb026e7a0b7f9f4be
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-04-11 14:57:53 +0800

    feat: jest单测配置

--- aliemente/src/App.vue
+++ aliemente/src/App.vue
@@ -1,35 +1,35 @@
 <template>
- <el-container>
-  <el-header>Header</el-header>
-  <el-main>Main</el-main>
-  <el-footer>Footer</el-footer>
-</el-container>
-  <hr>
+  <Fragment>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+    <hr />
 
-<el-container>
-  <el-header>Header</el-header>
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-main>Main</el-main>
-  </el-container>
-</el-container>
-  <hr>
-<el-container>
-  <el-aside width="200px">Aside</el-aside>
-  <el-container>
-    <el-header>Header</el-header>
-    <el-main>Main</el-main>
-    <el-footer>Footer</el-footer>
-  </el-container>
-</el-container>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+    <hr />
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </Fragment>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <style>
-
-body{
-  width:1000px;
-  margin:10px auto;
+body {
+  width: 1000px;
+  margin: 10px auto;
 }
 .el-header,
 .el-footer {
