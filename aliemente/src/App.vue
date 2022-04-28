b5aab7e8cf4ed107d9a514ad26e06179c0aa86e3
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-04-28 23:08:23 +0800

    feat: form表单组件

--- aliemente/src/App.vue
+++ aliemente/src/App.vue
@@ -27,10 +27,67 @@
     <el-button type="success"> 按钮 </el-button>
     <el-button>按钮</el-button>
     <el-button size="large"> 按钮 </el-button>
+    <hr />
+    <div v-if="comp == 'form'">
+      <el-form ref="myForm" :model="model" :rules="rules">
+        <el-form-item label="用户名：" prop="username">
+          <el-input v-model="model.username" />
+        </el-form-item>
+        <el-form-item label="密码：" prop="password">
+          <el-input v-model="model.password" type="password" />
+        </el-form-item>
+        <br />
+        <el-form-item>
+          <el-button type="primary" @click="login"> 登 录 </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </Fragment>
 </template>
 <script setup lang="ts">
 import ElButton from "./components/button/Button.vue";
+import { ref, reactive } from "vue";
+
+import { FormType } from "./components/form/type";
+
+const comp = ref("form");
+const compExamples = ref([
+  "container",
+  "button",
+  "form",
+  "dialog",
+  "notification",
+]);
+const showDialog = ref<boolean>(false);
+const model = reactive({
+  username: "",
+  password: "",
+});
+const rules = reactive({
+  username: [
+    {
+      required: true,
+      message: "请输入用户名！",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "请输入密码！",
+    },
+  ],
+});
+// 获取表单实例
+const myForm = ref<FormType>();
+const login = () => {
+  myForm.value?.validate((isValid) => {
+    if (isValid) {
+      console.log(model);
+    } else {
+      alert("请正确填写表单！");
+    }
+  });
+};
 </script>
 <style>
 body {
